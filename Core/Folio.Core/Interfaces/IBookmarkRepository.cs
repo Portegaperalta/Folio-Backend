e3d30925907f33a3b72c5e4283045505f8cab7bb
Pablo <ortegaperaltap@gmail.com>
2026-01-23 00:44:29 -0400

    Create interface IBookmarkRepository

---
+++ Core/Folio.Core/Interfaces/IBookmarkRepository.cs
@@ -0,0 +1,15 @@
+﻿using Folio.Core.Domain;
+
+namespace Folio.Core.Interfaces
+{
+    public interface IBookmarkRepository
+    {
+        Task<IEnumerable<Bookmark>> GetAllAsync(int folderId);
+        Task<Bookmark?> GetByIdAsync(int bookmarkId);
+        Task AddAsync(Bookmark bookmarkEntity);
+        Task UpdateAsync(Bookmark bookmarkEntity);
+        Task DeleteAsync(Bookmark bookmarkEntity);
+        Task<bool> ExistsAsync(int folderId);
+        Task<int> CountByFolderAsync(int folderId);
+    }
+}
