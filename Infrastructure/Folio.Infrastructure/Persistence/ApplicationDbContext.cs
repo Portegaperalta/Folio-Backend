a97381212197c900ec8b0e91123819402a7ba807
Pablo <ortegaperaltap@gmail.com>
2026-01-24 10:39:30 -0400

    Install necessary EF Core packages in infrastructure proyect and create ApplicationDbContext

---
+++ Infrastructure/Folio.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Folio.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Folio.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        DbSet<Folder> Folders { get; set; }
+        DbSet<Bookmark> Bookmarks { get; set; }
+    }
+}
