f0067b0fa34026115856ae6aeaea861c84bce072
Pablo <ortegaperaltap@gmail.com>
2026-01-24 22:53:19 -0400

    Create BookmarkService

---
+++ Core/Folio.Core/Application/Services/BookmarkService.cs
@@ -0,0 +1,141 @@
+﻿using Folio.Core.Domain;
+using Folio.Core.Interfaces;
+
+namespace Folio.Core.Application.Services
+{
+    public class BookmarkService
+    {
+        private readonly IBookmarkRepository _bookmarkRepository;
+
+        public BookmarkService(IBookmarkRepository bookmarkRepository)
+        {
+            _bookmarkRepository = bookmarkRepository;
+        }
+
+        public async Task<IEnumerable<Bookmark>> GetAllUserBookmarksAsync(int folderId)
+        {
+            return await _bookmarkRepository.GetAllAsync(folderId);
+        }
+
+        public async Task<Bookmark?> GetUserBookmarkByIdAsync(Guid bookmarkId)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                return null;
+            }
+
+            return bookmark;
+        }
+
+        public async Task CreateUserBookmarkAsync(Bookmark bookmarkEntity)
+        {
+            if (bookmarkEntity is null)
+            {
+                ArgumentNullException.ThrowIfNull("Bookmark entity cannot be null");
+            }
+
+            await _bookmarkRepository.AddAsync(bookmarkEntity!);
+        }
+
+        public async Task UpdateUserBookmarkAsync(Bookmark bookmarkEntity)
+        {
+            if (bookmarkEntity is null)
+            {
+                ArgumentNullException.ThrowIfNull("Bookmark entity cannot be null");
+            }
+
+            bool bookmarkExists = await _bookmarkRepository.ExistsAsync(bookmarkEntity!.Id);
+
+            if (bookmarkExists is false)
+            {
+                throw new ArgumentException($"Bookmark with id:{bookmarkEntity.Id} not found");
+            }
+
+            await _bookmarkRepository.UpdateAsync(bookmarkEntity);
+        }
+
+        public async Task DeleteUserBookmarkAsync(Guid bookmarkId)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            await _bookmarkRepository.DeleteAsync(bookmark);
+        }
+
+        public async Task ChangeUserBookmarkName(Guid bookmarkId,string newBookmarkName)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            bookmark.ChangeName(newBookmarkName);
+
+            await _bookmarkRepository.UpdateAsync(bookmark);
+        }
+
+        public async Task ChangeUserBookmarkUrl(Guid bookmarkId,string newBookmarkUrl)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            bookmark.ChangeUrl(newBookmarkUrl);
+
+            await _bookmarkRepository.UpdateAsync(bookmark);
+        }
+
+        public async Task MarkUserBookmarkAsFavorite(Guid bookmarkId)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            bookmark.MarkFavorite();
+
+            await _bookmarkRepository.UpdateAsync(bookmark);
+        }
+
+        public async Task UnmarkUserBookmarkAsFavorite(Guid bookmarkId)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            bookmark.UnmarkFavorite();
+
+            await _bookmarkRepository.UpdateAsync(bookmark);
+        }
+
+        public async Task MarkUserBookmarkAsVisitedAsync(Guid bookmarkId)
+        {
+            var bookmark = await _bookmarkRepository.GetByIdAsync(bookmarkId);
+
+            if (bookmark is null)
+            {
+                throw new ArgumentException($"Bookmark with id {bookmarkId} not found");
+            }
+
+            bookmark.Visit();
+
+            await _bookmarkRepository.UpdateAsync(bookmark);
+        }
+    }
+}
