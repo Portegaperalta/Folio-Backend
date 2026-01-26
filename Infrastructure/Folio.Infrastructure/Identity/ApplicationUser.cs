fd695738742191527e15f70c1664796f20013cf9
Pablo <ortegaperaltap@gmail.com>
2026-01-26 10:31:15 -0400

    create folder Indetity and class ApplicationUser

---
+++ Infrastructure/Folio.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Folio.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
