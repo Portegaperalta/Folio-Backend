2407f367ead446fcd686a8d20d9dd94901312d5d
Pablo <ortegaperaltap@gmail.com>
2026-01-26 22:01:37 -0400

    Create class TokenGenerator with method GenerateJwt

---
+++ Infrastructure/Folio.Infrastructure/Identity/TokenGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Folio.Core.Domain;
+using Folio.Core.Interfaces;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Folio.Infrastructure.Identity
+{
+    public sealed class TokenGenerator : ITokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public TokenGenerator(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public string GenerateJwt(User userEntity)
+        {
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, userEntity.Id.ToString()),
+                new(JwtRegisteredClaimNames.Email, userEntity.Email),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var securityToken = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpiryMinutes),
+                signingCredentials: credentials
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+            return token;
+        }
+    }
+}
