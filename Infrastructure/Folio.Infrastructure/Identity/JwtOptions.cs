b58203519b182b2cd09ed889684da9c796cd786f
Pablo <ortegaperaltap@gmail.com>
2026-01-26 20:24:14 -0400

    Create class JwtOptions

---
+++ Infrastructure/Folio.Infrastructure/Identity/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Folio.Infrastructure.Identity
+{
+    public class JwtOptions
+    {
+        public string Issuer { get; init; } = null!;
+        public string Audience { get; init; } = null!;
+        public string SecretKey { get; init; } = null!;
+        public int ExpiryMinutes { get; init; }
+    }
+}
