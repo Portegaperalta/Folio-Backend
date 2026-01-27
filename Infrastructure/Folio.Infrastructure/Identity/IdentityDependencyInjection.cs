104ae1175899e6904f3b664b411047a66a59163c
Pablo <ortegaperaltap@gmail.com>
2026-01-26 20:01:11 -0400

    Create class IdentityDependencyInjection

---
+++ Infrastructure/Folio.Infrastructure/Identity/IdentityDependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Folio.Core.Interfaces;
+using Folio.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Folio.Infrastructure.Identity
+{
+    public static class IdentityDependencyInjection
+    {
+        public static IServiceCollection AddIdentityInfrastructure
+            (this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
