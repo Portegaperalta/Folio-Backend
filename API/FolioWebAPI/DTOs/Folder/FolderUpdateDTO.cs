cb863b1bb66506fc36e612c197e5d7cfabddc8fc
Pablo <ortegaperaltap@gmail.com>
2026-01-27 21:20:42 -0400

    Create FolderUpdateDTO

---
+++ API/FolioWebAPI/DTOs/Folder/FolderUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FolioWebAPI.DTOs.Folder
+{
+    public class FolderUpdateDTO
+    {
+        public string? Name { get; set; }
+        public bool? IsMarkedFavorite { get; set; }
+    }
+}
