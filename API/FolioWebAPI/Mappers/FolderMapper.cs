1744232cd4ec861fe1945cf9b3b13b4d05d41676
Pablo <ortegaperaltap@gmail.com>
2026-01-27 22:20:18 -0400

    Create custom mapper for Folder

---
+++ API/FolioWebAPI/Mappers/FolderMapper.cs
@@ -0,0 +1,25 @@
+﻿using FolioWebAPI.DTOs.Folder;
+using Folio.Core.Domain;
+
+namespace FolioWebAPI.Mappers
+{
+    public class FolderMapper
+    {
+        public FolderDTO ToDto(Folder folderEntity)
+        {
+            return new FolderDTO
+            {
+                Id = folderEntity.Id,
+                Name = folderEntity.Name,
+                IsMarkedFavorite = folderEntity.IsMarkedFavorite,
+                CreationDate = folderEntity.CreationDate,
+                LastVisitedTime = folderEntity.LastVisitedTime
+            };
+        }
+
+        public Folder ToEntity(int userId, FolderCreationDTO folderCreationDTO)
+        {
+            return new Folder(folderCreationDTO.Name, userId);
+        }
+    }
+}
