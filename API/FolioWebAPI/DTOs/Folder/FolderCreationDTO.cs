9f9f371245b0604e7b48e55d1b3df0638aea8521
Pablo <ortegaperaltap@gmail.com>
2026-01-27 21:11:49 -0400

    Create DTO FolderCreationDTO

---
+++ API/FolioWebAPI/DTOs/Folder/FolderCreationDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FolioWebAPI.DTOs.Folder
+{
+    public class FolderCreationDTO
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must have {1} characters or less")]
+        public required string Name { get; set; }
+    }
+}
