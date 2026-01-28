f036f7856f70d9750867593f7fd161f133d36b6a
Pablo <ortegaperaltap@gmail.com>
2026-01-27 22:44:07 -0400

    Creat mapper for Bookmark

---
+++ API/FolioWebAPI/Mappers/BookmarkMapper.cs
@@ -0,0 +1,29 @@
+﻿using Folio.Core.Domain;
+using FolioWebAPI.DTOs.Bookmark;
+
+namespace FolioWebAPI.Mappers
+{
+    public class BookmarkMapper
+    {
+        public BookmarkDTO ToDto(Bookmark bookmarkEntity)
+        {
+            return new BookmarkDTO
+            {
+                Id = bookmarkEntity.Id,
+                Name = bookmarkEntity.Name,
+                Url = bookmarkEntity.Url,
+                IsMarkedFavorite = bookmarkEntity.IsMarkedFavorite,
+                CreationDate = bookmarkEntity.CreationDate,
+                LastVisitedTime = bookmarkEntity.LastVisitedTime
+            };
+        }
+
+        public Bookmark ToEntity(int userId,BookmarkCreationDTO bookmarkCreationDTO)
+        {
+            return new Bookmark(bookmarkCreationDTO.Name, 
+                                bookmarkCreationDTO.Url,
+                                bookmarkCreationDTO.FolderId,
+                                userId);
+        }
+    }
+}
