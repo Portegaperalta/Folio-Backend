ed28e42c4eb2aba0b8a3a4796440c6536e0538c1
Pablo <ortegaperaltap@gmail.com>
2026-01-27 21:48:49 -0400

    create DTO for bookmark creation

---
+++ API/FolioWebAPI/DTOs/Bookmark/BookmarkCreationDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FolioWebAPI.DTOs.Bookmark
+{
+    public class BookmarkCreationDTO
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(150, ErrorMessage = "The field {0} must have {1} characters or less")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Url(ErrorMessage = "The field {0} must be a valid Url")]
+        public required string Url { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public required int FolderId { get; set; }
+    }
+}
