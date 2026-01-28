5705ea11011bf7c75f2a87ab0e7ee2a73f96a8de
Pablo <ortegaperaltap@gmail.com>
2026-01-27 21:41:49 -0400

    Create BookmarkDTO

---
+++ API/FolioWebAPI/DTOs/Bookmark/BookmarkDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FolioWebAPI.DTOs.Bookmark
+{
+    public class BookmarkDTO
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Url { get; set; }
+        public required bool IsMarkedFavorite { get; set; }
+        public required DateTime CreationDate { get; set; }
+        public required DateTime? LastVisitedTime { get; set; }
+    }
+}
