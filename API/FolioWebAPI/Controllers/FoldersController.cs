b6782b4f3480849db103635600d184dab657bd5c
Pablo <ortegaperaltap@gmail.com>
2026-01-28 23:26:30 -0400

    Create FoldersController and GetAll action

--- API/FolioWebAPI/Controllers/FoldersController.cs
+++ API/FolioWebAPI/Controllers/FoldersController.cs
@@ -1,4 +1,5 @@
 ﻿using Folio.Core.Application.Services;
+using Folio.Core.Interfaces;
 using FolioWebAPI.DTOs.Folder;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,29 @@ namespace FolioWebAPI.Controllers
     [Route("api/folders")]
     public class FoldersController : ControllerBase
     {
-        private readonly FolderService folderService;
+        private readonly FolderService _folderService;
+        private readonly ICurrentUserService _currentUserService;
 
-        public FoldersController(FolderService folderService)
+        public FoldersController(FolderService folderService, ICurrentUserService currentUserService)
         {
-            this.folderService = folderService;
+            _folderService = folderService;
+            _currentUserService = currentUserService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<FolderDTO>>> GetAll()
+        {
+            var currentUser = await _currentUserService.GetCurrentUserAsync();
+
+            if (currentUser is null)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            var folders = await _folderService.GetAllUserFoldersAsync(currentUser.Id);
+
+            return Ok(folders);
         }
     }
 }
