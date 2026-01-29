5b99d725ac8a7c5bcf2ca57704a44027977a86c1
Pablo <ortegaperaltap@gmail.com>
2026-01-28 22:24:06 -0400

    Create CurrentUserService class in API layer

---
+++ API/FolioWebAPI/Services/CurrentUserService.cs
@@ -0,0 +1,72 @@
+﻿using Folio.Core.Domain;
+using Folio.Core.Interfaces;
+using Folio.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace FolioWebAPI.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor, UserManager<ApplicationUser> userManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+
+        public int GetCurrentUserId()
+        {
+            var userIdClaim = _httpContextAccessor.HttpContext!
+                                                 .User
+                                                 .Claims
+                                                 .Where(c => c.Type == "id")
+                                                 .FirstOrDefault();
+
+            if (userIdClaim is null)
+            {
+                throw new UnauthorizedAccessException("User not authenticated");
+            }
+
+            return int.Parse(userIdClaim.Value);
+        }
+
+        public string GetCurrentUserEmail()
+        {
+            var userEmailClaim = _httpContextAccessor.HttpContext!
+                                                    .User
+                                                    .Claims
+                                                    .Where(c => c.Type == "email")
+                                                    .FirstOrDefault();
+
+            if (userEmailClaim is null)
+            {
+                throw new UnauthorizedAccessException("User not authenticated");
+            }
+
+            return userEmailClaim.Value;
+        }
+
+        public async Task<User?> GetCurrentUserAsync()
+        {
+            var userEmailClaim = GetCurrentUserEmail();
+
+            if (userEmailClaim is null)
+            {
+                return null;
+            }
+
+            var applicationUser = await _userManager.FindByEmailAsync(GetCurrentUserEmail());
+
+            if (applicationUser is null || applicationUser.IsDeleted is true)
+            {
+                return null;
+            }
+
+            var user = UserMapper.ToDomainEntity(applicationUser);
+
+            return user;
+        }
+    }
+}
