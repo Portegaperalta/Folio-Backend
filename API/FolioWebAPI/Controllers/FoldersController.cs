92c6e855c51057072cee1b65bb2c6edc05017291
Pablo <ortegaperaltap@gmail.com>
2026-01-28 20:40:08 -0400

    Create LoginCredentialsDTO

---
+++ API/FolioWebAPI/Controllers/FoldersController.cs
@@ -0,0 +1,19 @@
+﻿using Folio.Core.Application.Services;
+using FolioWebAPI.DTOs.Folder;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FolioWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/folders")]
+    public class FoldersController : ControllerBase
+    {
+        private readonly FolderService folderService;
+
+        public FoldersController(FolderService folderService)
+        {
+            this.folderService = folderService;
+        }
+    }
+}
---
+++ API/FolioWebAPI/DTOs/Auth/LoginCredentialsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FolioWebAPI.DTOs.Auth
+{
+    public class LoginCredentialsDTO
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} must be a valid email address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public required string Password { get; set; }
+    }
+}
