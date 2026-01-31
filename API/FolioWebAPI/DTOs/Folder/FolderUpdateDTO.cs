be4b26b33f9d15393a0991168191f2b73b9972a2
Pablo <ortegaperaltap@gmail.com>
2026-01-31 11:31:53 -0400

    rename error message for StringLengthData annotation

--- API/FolioWebAPI/DTOs/Folder/FolderUpdateDTO.cs
+++ API/FolioWebAPI/DTOs/Folder/FolderUpdateDTO.cs
@@ -5,7 +5,7 @@ namespace FolioWebAPI.DTOs.Folder
     public class FolderUpdateDTO
     {
         [Required(ErrorMessage = "The field {0} is required")]
-        [StringLength(maximumLength: 150, MinimumLength = 1, ErrorMessage = "The field {0} must have {1} characters or less")]
+        [StringLength(maximumLength: 150, MinimumLength = 1, ErrorMessage = "The length of field {0} must be between {2} and {1}")]
         public string? Name { get; set; }
         public bool? IsMarkedFavorite { get; set; }
     }
