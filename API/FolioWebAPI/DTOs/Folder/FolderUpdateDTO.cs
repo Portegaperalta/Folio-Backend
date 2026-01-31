c6b6c1339677e8d8870974f64afb3ae4f1ecb799
Pablo <ortegaperaltap@gmail.com>
2026-01-31 11:24:24 -0400

    Add required and stringLenth data annotations to Name attribute

--- API/FolioWebAPI/DTOs/Folder/FolderUpdateDTO.cs
+++ API/FolioWebAPI/DTOs/Folder/FolderUpdateDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace FolioWebAPI.DTOs.Folder
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FolioWebAPI.DTOs.Folder
 {
     public class FolderUpdateDTO
     {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(maximumLength: 150, MinimumLength = 1, ErrorMessage = "The field {0} must have {1} characters or less")]
         public string? Name { get; set; }
         public bool? IsMarkedFavorite { get; set; }
     }
