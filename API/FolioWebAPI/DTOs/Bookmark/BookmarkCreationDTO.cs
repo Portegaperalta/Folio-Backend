24909e217dd4c4f7cc8b82f5bb2d750573ea6f95
Pablo <ortegaperaltap@gmail.com>
2026-01-31 11:35:21 -0400

    Modify StringLength data annotation in name attribute for better clarity

--- API/FolioWebAPI/DTOs/Bookmark/BookmarkCreationDTO.cs
+++ API/FolioWebAPI/DTOs/Bookmark/BookmarkCreationDTO.cs
@@ -5,7 +5,7 @@ namespace FolioWebAPI.DTOs.Bookmark
     public class BookmarkCreationDTO
     {
         [Required(ErrorMessage = "The field {0} is required")]
-        [StringLength(150, ErrorMessage = "The field {0} must have {1} characters or less")]
+        [StringLength(maximumLength: 150, MinimumLength = 1, ErrorMessage = "The length of field {0} must be between {2} and {1}")]
         public required string Name { get; set; }
 
         [Required(ErrorMessage = "The field {0} is required")]
