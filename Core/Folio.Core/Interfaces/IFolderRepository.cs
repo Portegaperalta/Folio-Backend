552c2a5b4731a7c25f4370dbb2dda93643d665c0
Pablo <ortegaperaltap@gmail.com>
2026-01-31 22:05:35 -0400

    change GetByIdAsync parameter folderId type to Guid

--- Core/Folio.Core/Interfaces/IFolderRepository.cs
+++ Core/Folio.Core/Interfaces/IFolderRepository.cs
@@ -5,7 +5,7 @@ namespace Folio.Core.Interfaces
     public interface IFolderRepository
     {
         Task<IEnumerable<Folder>> GetAllAsync(Guid userId);
-        Task<Folder?> GetByIdAsync(Guid userId, int folderId);
+        Task<Folder?> GetByIdAsync(Guid userId, Guid folderId);
         Task AddAsync(Folder folderEntity);
         Task UpdateAsync(Folder folderEntity);
         Task DeleteAsync(Folder folderEntity);
