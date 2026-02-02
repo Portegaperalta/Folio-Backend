77ace2254f3fd006660ff2ca29d70a8891276741
Pablo <ortegaperaltap@gmail.com>
2026-02-01 23:36:51 -0400

    Create class level fields and TestInitialize method

--- Folio-Backend-Tests/Core/Application/Services/UnitTests/FolderServiceTests.cs
+++ Folio-Backend-Tests/Core/Application/Services/UnitTests/FolderServiceTests.cs
@@ -1,7 +1,27 @@
-﻿namespace Folio_Backend_Tests.Core.Application.Services.UnitTests
+﻿using Folio.Core.Application.Services;
+using Folio.Core.Domain;
+using Folio.Core.Interfaces;
+using NSubstitute;
+
+namespace Folio_Backend_Tests.Core.Application.Services.UnitTests
 {
     [TestClass]
     public class FolderServiceTests
     {
+        private Guid MockUserId = Guid.NewGuid();
+        private Guid MockFolderId = Guid.NewGuid();
+        private Folder MockFolderEntity = null!;
+        IFolderRepository MockfolderRepository = null!;
+        IEnumerable<Folder> MockFolderList = null!;
+        private FolderService folderService = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            MockFolderList = new List<Folder> { new Folder("folderMock", MockUserId) };
+            MockfolderRepository = Substitute.For<IFolderRepository>();
+            MockFolderEntity = Substitute.For<Folder>("folderMock",MockUserId);
+            folderService = new FolderService(MockfolderRepository);
+        }
     }
 }
