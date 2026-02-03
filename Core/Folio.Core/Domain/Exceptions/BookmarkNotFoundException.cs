bfe47356ec238cf9fefe4b66bb8fceefc0182f33
Pablo <ortegaperaltap@gmail.com>
2026-02-02 22:35:22 -0400

    Create custom exception BookmarkNotFoundException

---
+++ Core/Folio.Core/Domain/Exceptions/BookmarkNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Folio.Core.Domain.Exceptions
+{
+    public class BookmarkNotFoundException : Exception
+    {
+        public BookmarkNotFoundException(Guid bookmarkId)
+            : base($"Bookmark with id {bookmarkId} not found") { }
+    }
+}
