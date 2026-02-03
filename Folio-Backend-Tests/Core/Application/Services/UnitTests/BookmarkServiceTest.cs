65106efd14f0388fb175e939668731905cd7583b
Pablo <ortegaperaltap@gmail.com>
2026-02-02 22:22:55 -0400

    Create test class BookmarkServiceTest with internal classes and Setup method

---
+++ Folio-Backend-Tests/Core/Application/Services/UnitTests/BookmarkServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Folio.Core.Application.Services;
+using Folio.Core.Domain.Entities;
+using Folio.Core.Interfaces;
+using NSubstitute;
+
+namespace Folio_Backend_Tests.Core.Application.Services.UnitTests
+{
+    [TestClass]
+    public class BookmarkServiceTest
+    {
+        private readonly Guid MockUserId = Guid.NewGuid();
+        private readonly Guid MockFolderId = Guid.NewGuid();
+        private readonly string FakeUrl = "https://fakeurl.com";
+        private Bookmark MockBookmarkEntity = null!;
+        IBookmarkRepository MockBookmarkRepository = null!;
+        IEnumerable<Bookmark> MockBookmarkList = null!;
+        private BookmarkService bookmarkService = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            MockBookmarkEntity = new("mockBookmark", FakeUrl, MockFolderId, MockUserId);
+            MockBookmarkRepository = Substitute.For<IBookmarkRepository>();
+            MockBookmarkList = new List<Bookmark> { MockBookmarkEntity };
+            bookmarkService = new(MockBookmarkRepository);
+        }
+    }
+}
