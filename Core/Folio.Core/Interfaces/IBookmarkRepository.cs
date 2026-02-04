bead7e7fd791d033f74b6bec886ecd7d8f104e2e
Pablo <ortegaperaltap@gmail.com>
2026-02-04 08:47:06 -0400

    implement interface methods GetAllByUserIdAsync and GetAllByUserAndFolderIdAsync in bookmark repository

--- Core/Folio.Core/Interfaces/IBookmarkRepository.cs
+++ Core/Folio.Core/Interfaces/IBookmarkRepository.cs
@@ -5,7 +5,6 @@ namespace Folio.Core.Interfaces
     public interface IBookmarkRepository
     {
         Task<IEnumerable<Bookmark>> GetAllByUserIdAsync(Guid userId);
-
         Task<IEnumerable<Bookmark>> GetAllByUserAndFolderIdAsync(Guid userId, Guid folderId);
         Task<Bookmark?> GetByIdAsync(Guid userId, Guid folderId, Guid bookmarkId);
         Task<Bookmark?> GetByIdAsNoTrackingAsync(Guid userId, Guid folderId, Guid bookmarkId);
