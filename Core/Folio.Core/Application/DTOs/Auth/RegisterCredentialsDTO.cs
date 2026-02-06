6c0da456e66a6bfcb6e1f1937970a3a45b062308
Pablo <ortegaperaltap@gmail.com>
2026-02-06 17:40:56 -0400

    add minimun length validation to password field

--- Core/Folio.Core/Application/DTOs/Auth/RegisterCredentialsDTO.cs
+++ Core/Folio.Core/Application/DTOs/Auth/RegisterCredentialsDTO.cs
@@ -14,6 +14,7 @@ namespace Folio.Core.Application.DTOs.Auth
         public required string Email { get; set; }
 
         [Required(ErrorMessage = "The field {0} is required")]
+        [MinLength(8, ErrorMessage = "The field {0} must be at least {1} characters long")]
         public required string Password { get; set; }
 
         [Phone(ErrorMessage = "The field {0} must be a valid phone number")]
