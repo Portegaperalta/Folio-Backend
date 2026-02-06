08e4b97f9a88d0c26672580379fb957110749775
Pablo <ortegaperaltap@gmail.com>
2026-02-06 18:56:58 -0400

    Create custom validation SpecialCharacterAttribute to check if a value contains any special characters

---
+++ Core/Folio.Core/Application/Attributes/SpecialCharacterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Buffers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Folio.Core.Application.Attributes
+{
+    public class SpecialCharacterAttribute : ValidationAttribute
+    {
+        private static readonly SearchValues<char> _SpecialCharacterSearchValues = SearchValues.Create("!@#$%^&()_+-={}[]|\\:;\"'<>,.?/");
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is null || string.IsNullOrEmpty(value.ToString()))
+                return ValidationResult.Success;
+
+            var valueString = value.ToString();
+
+            if (valueString.ContainsAny(_SpecialCharacterSearchValues) is false)
+            {
+                return new ValidationResult("Password must contain at least one special character");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
