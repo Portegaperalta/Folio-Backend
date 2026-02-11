40a44e43228220680c1856dafd5c4f910734d5e1
Pablo <ortegaperaltap@gmail.com>
2026-02-10 21:29:15 -0400

    Create UserRepository

---
+++ Infrastructure/Folio.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Folio.Core.Domain.Entities;
+using Folio.Core.Interfaces;
+using Folio.Infrastructure.Identity.Mappers;
+using Folio.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Folio.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            var applicationUser = await _dbContext.Users
+                                                  .Where(u => u.Id == userId)
+                                                  .FirstOrDefaultAsync();
+
+            return applicationUser != null
+                ? UserMapper.ToDomainEntity(applicationUser)
+                : null;
+        }
+
+        public async Task UpdateUserAsync(User userEntity)
+        {
+            var applicationUser = UserMapper.ToApplicationUser(userEntity);
+
+            _dbContext.Users.Update(applicationUser);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(User userEntity)
+        {
+            var applicationUser = UserMapper.ToApplicationUser(userEntity);
+
+            _dbContext.Users.Remove(applicationUser);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
