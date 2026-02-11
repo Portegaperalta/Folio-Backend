96afa5be338f9a7e6bbfe2a8445bc1f9b26c2116
Pablo <ortegaperaltap@gmail.com>
2026-02-10 21:57:12 -0400

    Create UserUpdateDTO

---
+++ Core/Folio.Core/Application/DTOs/User/UserUpdateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Folio.Core.Application.DTOs.User
+{
+    public class UserUpdateDTO
+    {
+        [MaxLength(250, ErrorMessage = "The field {0} must have {1} characters or less")]
+        [MinLength(1, ErrorMessage = "The field {0} must have {0} or more")]
+        public string? Name { get; private set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} must be a valid email address")]
+        public string? Email { get; private set; }
+
+        [Phone(ErrorMessage = "The field {0} must be a valid phone number")]
+        public string? PhoneNumber { get; private set; }
+    }
+}
