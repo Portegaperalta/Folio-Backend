0d466f354147cf9c0d357ac35f507a8774283d22
Pablo <ortegaperaltap@gmail.com>
2026-02-11 21:46:47 -0400

    Create and implement interface for BookmarkService

---
+++ Core/Folio.Core/Interfaces/IBookmarkService.cs
@@ -0,0 +1,15 @@
+﻿using Folio.Core.Application.DTOs.Bookmark;
+
+namespace Folio.Core.Interfaces
+{
+    public interface IBookmarkService
+    {
+        Task<IEnumerable<BookmarkDTO>> GetAllBookmarksAsync(Guid userId, Guid? folderId);
+        Task<BookmarkDTO?> GetBookmarkByIdAsync(Guid userId, Guid folderId, Guid bookmarkId);
+        Task<int> CountBookmarksByFolderIdAsync(Guid userId, Guid folderId);
+        Task<BookmarkDTO> CreateBookmarkAsync(Guid userId, Guid folderId, BookmarkCreationDTO bookmarkCreationDTO);
+        Task UpdateBookmarkAsync(Guid userId, Guid folderId, BookmarkUpdateDTO bookmarkUpdateDTO);
+        Task<bool> DeleteBookmarkAsync(Guid userId, Guid folderId, Guid bookmarkId);
+        Task MarkBookmarkAsVisitedAsync(Guid userId, Guid folderId, Guid bookmarkId);
+    }
+}
