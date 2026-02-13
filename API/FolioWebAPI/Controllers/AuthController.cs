b28bf3238d4065d31b643bf9a3c399a8516a988d
Pablo <ortegaperaltap@gmail.com>
2026-02-12 22:04:15 -0400

    Wrap Login authenticationResponseDTO inside OK action result

--- API/FolioWebAPI/Controllers/AuthController.cs
+++ API/FolioWebAPI/Controllers/AuthController.cs
@@ -48,7 +48,7 @@ namespace FolioWebAPI.Controllers
                 return Unauthorized("Authentication failed");
             }
 
-            return authenticationResponseDTO;
+            return Ok(authenticationResponseDTO);
         }
 
         [HttpGet("renew-token")]
