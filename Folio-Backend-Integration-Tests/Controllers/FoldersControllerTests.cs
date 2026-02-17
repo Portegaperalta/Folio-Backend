a8f104a147e7fe68b66cf96155320281529c680c
Pablo <ortegaperaltap@gmail.com>
2026-02-16 21:56:02 -0400

    Create test class for Folders controller

---
+++ Folio-Backend-Integration-Tests/Controllers/FoldersControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Folio_Backend_Integration_Tests.Utils;
+using FolioWebAPI;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text.Json;
+using Xunit;
+
+namespace Folio_Backend_Integration_Tests.Controllers
+{
+    public class FoldersControllerTests : TestsBase
+    {
+
+        private readonly string baseUrl = "/api/folders";
+        private readonly string dbName = Guid.NewGuid().ToString();
+
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+        private readonly WebApplicationFactory<Program> _webApplicationFactory;
+        private readonly HttpClient _client;
+        
+        public FoldersControllerTests()
+        {
+            _jsonSerializerOptions = _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            _webApplicationFactory = BuildWebApplicationFactory(dbName);
+            _client = _webApplicationFactory.CreateClient();
+        }
+    }
+}
