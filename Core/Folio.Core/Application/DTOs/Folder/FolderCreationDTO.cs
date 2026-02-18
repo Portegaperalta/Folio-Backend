6ebff6bf26b1028362fa1b1dd632f8e480fe80f2
Pablo <ortegaperaltap@gmail.com>
2026-02-17 21:33:33 -0400

    remove redundand string length validation

--- Core/Folio.Core/Application/DTOs/Folder/FolderCreationDTO.cs
+++ Core/Folio.Core/Application/DTOs/Folder/FolderCreationDTO.cs
@@ -5,7 +5,6 @@ namespace Folio.Core.Application.DTOs.Folder
     public class FolderCreationDTO
     {
         [Required(ErrorMessage = "The field {0} is required")]
-        [StringLength(maximumLength: 150, MinimumLength = 1, ErrorMessage = "The length of field {0} must be between {2} and {1}")]
         public required string Name { get; set; }
     }
 }
