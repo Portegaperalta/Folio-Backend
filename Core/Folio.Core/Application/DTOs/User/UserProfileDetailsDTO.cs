a22af8ca4b420650bb282e3c794d201695fbf912
Pablo <ortegaperaltap@gmail.com>
2026-02-19 21:37:50 -0400

    Create DTO for user profile details

---
+++ Core/Folio.Core/Application/DTOs/User/UserProfileDetailsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Folio.Core.Application.DTOs.User
+{
+    public class UserProfileDetailsDTO
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
