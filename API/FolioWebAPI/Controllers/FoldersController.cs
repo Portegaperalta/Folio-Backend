cb70a734440b61550962774fd9a8aef5afefb221
Pablo <ortegaperaltap@gmail.com>
2026-02-20 22:08:42 -0400

    Pass paginationDTO to service call

--- API/FolioWebAPI/Controllers/FoldersController.cs
+++ API/FolioWebAPI/Controllers/FoldersController.cs
@@ -31,7 +31,7 @@ namespace FolioWebAPI.Controllers
             if (currentUser is null)
                 return Unauthorized("Authorization failed");
 
-            var foldersDTOs = await _folderService.GetAllFoldersDTOsAsync(currentUser.Id);
+            var foldersDTOs = await _folderService.GetAllFoldersDTOsAsync(currentUser.Id, paginationDTO);
 
             return Ok(foldersDTOs);
         }
